98f93717c993d2e304b00229eb7e4fe41e3f648b
wortex1232 <transsercan@gmail.com>
2026-05-08 22:09:45 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const mineflayer = require('mineflayer');
 
 const botConfig = {
-    host: 'Voix317.aternos.me',
-    port: 51394,
+    host: 'mtatr.aternos.me',
+    port: 25565,
     version: '1.21.11',
     checkTimeoutInterval: 60000
 };
